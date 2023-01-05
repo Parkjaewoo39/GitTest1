f46720e27731773d4e5e9d9033477ae5c5214ebc
Giole13 <119779618+Giole13@users.noreply.github.com>
2023-01-05 11:05:53 +0900

    [jwPark] style(Ruler) modified class position

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,22 @@
 ﻿using System;
 namespace ConsoleApp1
 {
+    // {        2023.01.05. Modified class position / Alpha
+    public class Rular
+    {
+        private const float ONE_INCH = 2.54f;
+        public int Centimeter { get; set; } = 0;
+        public float Inch
+        {
+            get { return Centimeter * ONE_INCH; }
+            private set { Centimeter = (int)(value / ONE_INCH); }
+        }
+        public Rular(int cmValue) { Centimeter = cmValue; }
+        public void Run()
+        {
+            Console.WriteLine($"{Centimeter}cm 는 {Inch}inch 입니다. ");
+        }
+    }   //class Ruler 2023.01.05. Modified class position / Alpha
     public class Program
     {
         static void Main(string[] args)
@@ -10,19 +26,5 @@ namespace ConsoleApp1
         }   //Main
     }   //Program
 
-    public class Rular 
-    {
-        private const float ONE_INCH = 2.54f;
-        public int Centimeter { get; set; } = 0;
-        public float Inch 
-        {
-            get { return Centimeter *ONE_INCH; }
-            private set { Centimeter = (int)(value / ONE_INCH); }
-        }
-        public Rular(int cmValue) { Centimeter = cmValue; }
-        public void Run() 
-        {
-            Console.WriteLine($"{Centimeter}cm 는 {Inch}inch 입니다. ");
-        }
-    }   //class Ruler
+    
 }
