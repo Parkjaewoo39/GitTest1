a2918c7854911afb1a2e6ae5bfeb64b818f9753a
Giole13 <119779618+Giole13@users.noreply.github.com>
2023-01-05 09:45:40 +0900

    [jwPark] Rular project setup ok

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace ConsoleApp1
+﻿using System;
+namespace ConsoleApp1
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Rular rular = new Rular(10);
+            rular.Run();
+        }   //Main
+    }   //Program
+
+    public class Rular 
+    {
+        private const float ONE_INCH = 2.54f;
+        public int Centimeter { get; set; } = 0;
+        public float Inch 
+        {
+            get { return Centimeter *ONE_INCH; }
+            private set { Centimeter = (int)(value / ONE_INCH); }
         }
-    }
+        public Rular(int cmValue) { Centimeter = cmValue; }
+        public void Run() 
+        {
+            Console.WriteLine($"{Centimeter}cm 는 {Inch}inch 입니다. ");
+        }
+    }   //class Ruler
 }
